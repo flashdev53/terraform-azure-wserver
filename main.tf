fb1e207857af6561bc5fdf03ef9da6961f7c78e5
flashdev53 <adevaraj53@gmail.com>
2025-08-20 22:19:46 +0000

    update public_key

--- main.tf
+++ main.tf
@@ -44,7 +44,7 @@ resource "azurerm_linux_virtual_machine" "vm"{
 
     admin_ssh_key {
         username="devadmin"
-        public_key=file("~/.ssh/id_rsa.pub")
+        public_key = file("/home/dev/.ssh/id_rsa.pub")
     }
 
     os_disk {
