54275d9aad0e7ff954b828a4795fd4a09ccb7a80
flashdev53 <adevaraj53@gmail.com>
2025-08-20 22:34:21 +0000

    dynamic ip to static

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "azurerm_subnet" "subnet"{
 }
 resource "azurerm_public_ip" "publicip"{
     name="dev-publicip"
-    allocation_method="Dynamic"
+    allocation_method="Static"
     location=azurerm_resource_group.rg.location
     resource_group_name=azurerm_resource_group.rg.name
 }
